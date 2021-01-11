64f0a4960cf5cddbfe0034d4e0a49815633ed9d0
Steve <06tven14@gmail.com>
2021-01-12 00:49:46 +0800

    Initial

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Exceptions\ProductNotBelongsToUser;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\Product\ProductCollection;
+use App\Http\Requests\ProductRequest;
+use Facade\FlareClient\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class ProductController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ProductCollection::collection(Product::paginate(20));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductRequest $request)
+    {
+        $product = new Product;
+        $product->user_id = Auth::id();
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+        $product->save();
+        return response([
+            'data' => new ProductResource($product)
+        ],201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $this->ProductUserCheck($product);
+        $request['detail'] = $request->description;
+        // unset($request['description']);
+        $request['name'] = $request->name;
+        $request['stocks'] = $request->stocks;
+        $request['price'] = $request->price;
+        $request['discount'] = $request->discount;
+        $product->update($request->all());
+        return response([
+            'data' => new ProductResource($product)
+        ],201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $this->ProductUserCheck($product);
+        $product->delete();
+
+        return response(null,204);
+    }
+
+    public function ProductUserCheck($product)
+    {
+        if (Auth::id() !== $product->user_id) {
+            throw new ProductNotBelongsToUser;
+        }
+
+    }
+
+}
---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Exceptions\ProductNotBelongsToUser;
+use App\Models\Review;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Resources\ReviewResource;
+use App\Http\Requests\ReviewRequest;
+use Facade\FlareClient\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class ReviewController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show','');
+    }
+     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Product $product)
+    {
+        return ReviewResource::collection($product->reviews);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ReviewRequest $request, Product $product)
+    {
+
+        $review = new Review();
+        $review->customer_id = Auth::id();
+        $review->star = $request->star;
+        $review->review = $request->review;
+        // $request->all();
+        // return $review;
+        $product->reviews()->save($review);
+        return response([
+            'data' => new ReviewResource($review)
+        ],201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Review  $review
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Review $review)
+    {
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\Review  $review
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Review $review)
+    {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Review  $review
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product, Review $review)
+    {
+        $this->ProductUserCheck($review);
+        $review['star'] = $request->star;
+        $request['review'] = $request->review;
+        $review->update($request->all());
+        // return $review;
+        return response([
+            'data' => new ReviewResource($review)
+        ],201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Review  $review
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product, Review $review)
+    {
+        $this->ProductUserCheck($review);
+        $review->delete();
+        return response([
+            'data' => new ReviewResource($review)
+        ],204);
+    }
+
+    public function ProductUserCheck($review)
+    {
+        if (Auth::id() !== $review->customer_id) {
+            throw new ProductNotBelongsToUser;
+        }
+
+    }
+
+
+
+}
