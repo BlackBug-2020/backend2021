16625db7e62224d6ed3967fc91e7084bb696fe6a
Black Bug <06tven14@gmail.com>
2021-02-11 01:48:24 -0800

    registration update 1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('gender');
+            $table->string('phone');
+            $table->string('barangay');
+            $table->string('Purok');
             $table->rememberToken();
             $table->timestamps();
         });
