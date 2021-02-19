5de55b31a0ea5662604f9f569e94ff0a01828ac3
Black Bug <06tven14@gmail.com>
2021-02-19 04:34:14 -0800

    19.2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,10 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('gender');
-            $table->string('phone');
-            $table->string('barangay');
-            $table->string('purok');
+            $table->string('gender')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('barangay')->nullable();
+            $table->string('purok')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
